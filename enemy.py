70d7c083b12254d21c27982e3a61d6436859e380
daring-board <iwasa@tasdg.co.jp>
2025-07-05 16:14:09 +0900

    feat: 敵が左右に移動するように修正 #1

--- enemy.py
+++ enemy.py
@@ -2,9 +2,16 @@
 import pygame
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, screen_width):
         super().__init__()
         self.image = pygame.Surface((50, 50))
         self.image.fill((0, 0, 255))  # 青色の四角形
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.speed = 2
+        self.screen_width = screen_width
+
+    def update(self):
+        self.rect.x += self.speed
+        if self.rect.left < 0 or self.rect.right > self.screen_width:
+            self.speed = -self.speed
